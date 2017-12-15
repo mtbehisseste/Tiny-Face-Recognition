e568f623d69280a53ab68b3182d7d9503b5601da
ludueinstain <monkey860315@gmail.com>
2017-12-15 21:05:26 +0800

    Done initail recognition

--- webcam.py
+++ webcam.py
@@ -1,36 +1,51 @@
 import numpy as np
 import cv2
-from detection import Detector
-import face_detection
-import time
+import face_recognition as fr
 
 cap = cv2.VideoCapture(0)
 
 if cap.isOpened():
-    det_mod_path = 'det_nets_3.ckpt'
-    cal_mod_path = 'cal_nets_14.ckpt'
-    detector = Detector(det_mod_path, cal_mod_path)
-
-    while(True):
+    my_img = fr.load_image_file("./known/me.jpg")
+    my_face_encoding = fr.face_encodings(my_img)[0]
+    process_time = True
+    
+    while True:
         ret, frame = cap.read()  # each for method return and function return 
+        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
-        # det_mod_path = 'det_nets_3.ckpt'
-        # cal_mod_path = 'cal_nets_14.ckpt'
-        # detetor = Detector(det_mod_path, cal_mod_path)
+        if process_time:
+            face_locations = fr.face_locations(small_frame)
+            face_encodings = fr.face_encodings(small_frame, face_locations)
 
-        # Our operations on the frame come here
-        if ret is True:
-            face_detection.faceD(frame, detector)
-            # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            # cv2.imshow('face', res)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-        else:
-            print("err")
-            continue
+            face_name = []
+            for i in face_encodings:
+                match = fr.compare_faces([my_face_encoding], i, 0.4)
+
+                name = "Unknown"
+                if match[0]:
+                    name = "Jimmy"
+
+                face_name.append(name)
+
+        process_time = not process_time
+
+        for (top, right, bottom, left), name in zip(face_locations, face_name):
+            top *= 4
+            right *= 4
+            bottom *= 4
+            left *= 4
+            cv2.rectangle(frame, (left, top), (right, bottom), (0,255,0), 2)
+
+            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+        cv2.imshow("frame", frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
 else: 
     print ("Err opening camera.")
 
-# When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
